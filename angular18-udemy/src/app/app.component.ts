ac2e9d93bd1c738f55ce41ef7099277ec4cd9850
Akshay Palwe <90410114+AkshayRPalwe@users.noreply.github.com>
2024-08-20 09:53:33 +0000

    42. @if and @else

--- angular18-udemy/src/app/app.component.ts
+++ angular18-udemy/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { TasksComponent } from './tasks/tasks.component';
 export class AppComponent {
   users = DUMMY_USERS;
 
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return DUMMY_USERS.find((user) => user.id === this.selectedUserId);
