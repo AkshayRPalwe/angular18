8da43790c9e16178515207c8d68f6f8fa362d6a0
Akshay Palwe <90410114+AkshayRPalwe@users.noreply.github.com>
2024-08-20 07:57:03 +0000

    38. "?" vs "!"

--- angular18-udemy/src/app/tasks/tasks.component.ts
+++ angular18-udemy/src/app/tasks/tasks.component.ts
@@ -9,5 +9,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
+  @Input() name?: string;
+  // @Input() name: string | undefined;
 }
