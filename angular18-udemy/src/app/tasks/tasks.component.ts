46b115703289afa84cbc8adfda3c3e75ea3563ea
Akshay Palwe <90410114+AkshayRPalwe@users.noreply.github.com>
2024-08-23 17:00:16 +0000

    task component added.

--- angular18-udemy/src/app/tasks/tasks.component.ts
+++ angular18-udemy/src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { JsonPipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
 })
