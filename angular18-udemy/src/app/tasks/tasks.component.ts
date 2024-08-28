c7b2a0ef7d3c1199f0612f0a3cec699e3253b4d3
Akshay Palwe <90410114+AkshayRPalwe@users.noreply.github.com>
2024-08-28 00:53:44 +0000

    49: Completing/deleting tasks.

--- angular18-udemy/src/app/tasks/tasks.component.ts
+++ angular18-udemy/src/app/tasks/tasks.component.ts
@@ -41,7 +41,11 @@ export class TasksComponent {
 
   get selectedUserTasks() {
     console.log(this.tasks.filter((task) => task.userId === this.userId));
-    
+
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
