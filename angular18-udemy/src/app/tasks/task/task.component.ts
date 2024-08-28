dd9a86f791dcde6989d244a688e5672e6d78a271
Akshay Palwe <90410114+AkshayRPalwe@users.noreply.github.com>
2024-08-28 00:02:34 +0000

    46. Outputting task data in task component.

--- angular18-udemy/src/app/tasks/task/task.component.ts
+++ angular18-udemy/src/app/tasks/task/task.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
-
+  @Input({ required: true }) task!: Task;
+  
 }
